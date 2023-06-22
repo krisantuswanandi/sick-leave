dc56b81f7e91905a0bccde7dba8931046480ed08
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-22 23:14:56 +0700

    trigger sick leave workflow from ui

--- app/AddItem.tsx
+++ app/AddItem.tsx
@@ -1,11 +1,27 @@
 "use client"
 
+import { useState } from "react"
 import * as Dialog from "@radix-ui/react-dialog"
 import { Plus } from "lucide-react"
 
-export default function AddItem() {
+export default function AddItem(props: { submit: (password: string) => Promise<boolean> }) {
+  const [open, setOpen] = useState(false)
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit() {
+    const result = await props.submit(password)
+
+    if (result) {
+      setPassword("")
+      setOpen(false)
+    } else {
+      alert("Submit failed!!")
+      setPassword("")
+    }
+  }
+
   return (
-    <Dialog.Root>
+    <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
         <button className="fixed bottom-6 left-1/2 w-12 h-12 translate-x-[-50%] flex justify-center items-center bg-slate-950 border border-slate-800 rounded-md text-slate-500 hover:bg-slate-900 outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-200/5 focus-visible:outline-4">
           <Plus />
@@ -14,30 +30,32 @@ export default function AddItem() {
       <Dialog.Portal>
         <Dialog.Overlay className="bg-slate-950/50 fixed inset-0 backdrop-blur-sm" />
         <Dialog.Content className="fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] px-6 pb-6 rounded-md bg-slate-950 border border-slate-800 outline-none">
-          <div className="py-10">
-            <label htmlFor="password">
-              Enter password:
-            </label>
-            <fieldset className="mt-2">
-              <input
-                id="password"
-                type="password"
-                className="w-full text-xl py-1 px-4 bg-slate-950 border border-slate-800 rounded-md outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-200/5 focus-visible:outline-4"
-              />
-            </fieldset>
-          </div>
-          <div className="flex justify-end">
-            <Dialog.Close asChild>
-              <button className="px-4 py-1 mr-2 rounded-md text-slate-400 outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-300/25 focus-visible:outline-2">
-                Cancel
-              </button>
-            </Dialog.Close>
-            <Dialog.Close asChild>
+          <form action={handleSubmit}>
+            <div className="py-10">
+              <label htmlFor="password">
+                Enter password:
+              </label>
+              <fieldset className="mt-2">
+                <input
+                  id="password"
+                  type="password"
+                  className="w-full text-xl py-1 px-4 bg-slate-950 border border-slate-800 rounded-md outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-200/5 focus-visible:outline-4"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </fieldset>
+            </div>
+            <div className="flex justify-end">
+              <Dialog.Close asChild>
+                <button className="px-4 py-1 mr-2 rounded-md text-slate-400 outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-300/25 focus-visible:outline-2" type="button">
+                  Cancel
+                </button>
+              </Dialog.Close>
               <button className="bg-slate-200 hover:bg-slate-300 text-slate-950 px-4 py-1 rounded-md outline-none focus-visible:outline-offset-0 focus-visible:outline-blue-300/25 focus-visible:outline-4">
                 Submit
               </button>
-            </Dialog.Close>
-          </div>
+            </div>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,30 @@ const data = fs.readFileSync(path.join(process.cwd(), "data") + "/items.txt", "u
 const dates = data.split("\n").slice(0, -1)
 
 export default function Home() {
+  async function addItem(password: string) {
+    "use server"
+
+    if (password !== process.env.USER_PASSWORD) {
+      return false
+    }
+
+    const response = await fetch("https://api.github.com/repos/krisantuswanandi/sick-leave/actions/workflows/sick-leave.yml/dispatches", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        accept: "application/vnd.github+json",
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+      body: JSON.stringify({ ref: "main" }),
+    })
+
+    return response.ok
+  }
+
   return (
     <div className="flex">
       {dates.map(date => <Item key={date} label={format(new Date(date), "dd MMM yyyy")} />)}
-      <AddItem />
+      <AddItem submit={addItem} />
     </div>
   )
 }
